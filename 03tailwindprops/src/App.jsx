e86d65213c5946e49b8e64b84f7d03380072d9a4
Arham Shajahan <universalmadman@gmail.com>
2024-06-14 16:36:12 +0530

    just some changes

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -15,9 +15,6 @@ function App() {
     <h1 className='p-4 mb-4 text-black bg-green-400 rounded-xl'>Just Do It</h1>
     <Card channel = "Arham shajahan" myObj = "Click ME" />
     <Card channel = "Keerthivasan" myObj = "Push ME" />
-
-   
-
     
     </>
   )
--- 03tailwindprops/src/main.jsx
+++ 03tailwindprops/src/main.jsx
@@ -4,6 +4,11 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
+  // Purpose: This line of code initializes your React application and tells React where in the HTML to insert your app.
+  //  It links your React components to a specific part of your HTML and begins rendering your app, making it interactive and dynamic.
+  // the purpose of the above purpose to tell about the purpose of this area
+
   <React.StrictMode>
     <App />
   </React.StrictMode>,
